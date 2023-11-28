ddedaa025e3941ee9bda271a53317a9bce9249f4
Daniele Caramanica <danielecaramanica@gmail.com>
2023-11-28 16:53:51 +0100

    Aggiungere i file di progetto.

---
+++ ArraysParameters/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        int[] studentsGrade = { 9, 8, 8, 7, 9, 10, 7};
+        double averageResult = GetAverage(studentsGrade);
+
+        foreach( int grade in studentsGrade )
+        {
+            Console.WriteLine("{0}", grade);
+        }
+
+        Console.WriteLine("The average is {0}", averageResult);
+        Console.ReadKey();  
+
+
+
+
+    }
+
+    static double GetAverage(int[] gradesArray)
+    {
+        int size = gradesArray.Length;  
+        double sum = 0;
+        double average;
+
+        for(int i = 0; i < size; i++)
+        {
+            sum += gradesArray[i];
+
+        }
+
+
+        average =(double) sum / size;
+        return average;
+
+
+    }
+}
